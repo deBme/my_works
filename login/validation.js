c7df04c901eb81a9e1f465cd64ef58c4ad4216fd
me_deb <dbjtsrkr672@gmail.com>
2016-03-17 19:04:55 +0530

    minor updates

--- login/validation.js
+++ login/validation.js
@@ -5,14 +5,14 @@ function check(){
  var regus = /^[a-zA-Z_]{1,10}$/;
       if (regus.test(username.value)) {}
     else {
-document.getElementById('errfn').innerHTML="This is invalid name please note that username should contain a-ZA-Z_";
+document.getElementById('errfn').innerHTML="This is an invalid name! please note that username should contain a-ZA-Z";
         //alert("Check Your Username");return false;
     }
 
   var regph=/^(?=.*[0-9])(?=.*[a-z)(?=.*[A-Z])[a-zA-Z0-9*]{6,32}$/;
   if(regph.test(pass.value)){
     return true;
-  }else{document.getElementById("errps").innerHTML = "This is invalid password please note that password should contain atleast one a-ZA-Z0-9";
+  }else{document.getElementById("errps").innerHTML = "This is an invalid password! please note that password should contain atleast one a-ZA-Z0-9";
     //alert("Check Your password");
 	return false;
   }
